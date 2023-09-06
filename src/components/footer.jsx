3cd9c37293f6f10f70d2a3964b5973237569d636
Abdullah Shakeel <abdullahshakeel599@gmail.com>
2023-09-07 01:25:32 +0500

    Added Socials in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-scroll";
+import { SocialIcon } from "react-social-icons";
 
 class Footer extends Component {
   state = {};
@@ -10,6 +11,35 @@ class Footer extends Component {
           <span className="text-sm  sm:text-center  text-[color:var(--secondary-text-color)]">
             © 2023 Abdullah Shakeel . All Rights Reserved.
           </span>
+          <div className="flex items-center mt-4 space-x-4 md:mt-0 mx-2">
+             <SocialIcon
+            url="https://github.com/abdullah599"
+            className="m-4 "
+            target="_blank"
+            fgColor="#fff"
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/abdullah-shakeel-a77748224"
+            className="m-4"
+            target="_blank"
+            fgColor="#fff"
+            bgColor="#252525"
+          />
+          <SocialIcon
+            url="https://www.instagram.com/abdullah0___o/"
+            className="m-4"
+            target="_blank"
+            fgColor="#fff"
+            bgColor="#252525"
+          />
+          <SocialIcon
+            url="https://www.facebook.com/abdullahshakeel123"
+            className="m-4"
+            target="_blank"
+            fgColor="#fff"
+            bgColor="#252525"
+            />
+          </div>
           <ul className="flex flex-wrap items-center mt-3 sm:mt-0  text-[color:var(--secondary-text-color)]">
             <li>
               <Link
